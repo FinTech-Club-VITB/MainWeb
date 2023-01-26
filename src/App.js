9b771450f8cb1854441493624c95d2f3c87d182d
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-01-26 23:57:12 +0530

    Created the About Us page, still have some issues in responsiveness and hover effect

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import Home from "./components/home";
+import About from "./components/home/about";
 import Footer from "./components/structures/footer";
 import Navbar from "./components/structures/navbar";
 
 function App() {
   return (
     <div>
-      <Navbar/>
-      <Home/>
-      <Footer/>
+      <About/>
     </div>
   );
 }
--- src/components/home/about/index.js
+++ src/components/home/about/index.js
@@ -1,10 +1,46 @@
+import picture from './'
 import React from 'react'
 import './index.scss'
+import background from '../../../img/planet.png'
+import phone from '../../../img/phone-layers.png'
 
 const About = () => {
   return (
-    <div>About</div>
+    <>
+        <img class = "background" src={background} alt="back" />
+        <div class = "gradient">
+          </div>
+          <div class="about">
+          <h1>ABOUT US</h1>
+          <hr class="line"/>
+          <p class="tag">
+            We built your feuture
+          </p>
+          <p1 class="word">Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
+          Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type 
+          and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting, 
+          remaining essentially unchanged.
+          <br/><br/><br/>
+          It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages,
+          and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+          <br/><br/><br/>
+          Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
+           It has survived not only five centuries but also the leap into electronic typesetting, remaining essentially unchanged.
+          </p1>
+          <img class = "phone-layers" src={phone} data-value = "5" alt="phone" />
+        </div>
+  </>
   )
 }
 
+document.addEventListener("mousemove", parallax);
+function parallax(e){
+  document.querySelectorAll(".phone-layers").forEach(function(move){
+    var moving_value = move.getAttribute("data-value");
+    var x = (e.clientX*moving_value) /250;
+    var y = (e.clientY*moving_value) /250;
+
+    move.style.transform = "translateX(" + x + "px) translateY(" + y + "px)";
+  });
+}
 export default About
