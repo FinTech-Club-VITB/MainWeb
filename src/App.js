b36381de9cfc7108ccab5bcc2011db962d63fe58
NamelessExplorer <91269749+NamelessExplorer@users.noreply.github.com>
2023-02-05 12:44:05 +0530

    Created the landing page

--- src/App.js
+++ src/App.js
@@ -3,11 +3,12 @@ import About from "./components/home/about";
 import Footer from "./components/structures/footer";
 import Navbar from "./components/structures/navbar";
 import Teams from './components/home/team';
+import Hero from './components/home/hero';
 
 function App() {
   return (
     <div>
-      <Teams/>
+      <Hero/>
     </div>
   );
 }
