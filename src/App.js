099239d46307339c04bfec5efa4b8683defe83a3
retsambew <namanaskn2@gmail.com>
2023-02-09 01:57:28 +0530

    temp

--- src/App.js
+++ src/App.js
@@ -2,12 +2,11 @@ import Home from "./components/home";
 import About from "./components/home/about";
 import Footer from "./components/structures/footer";
 import Navbar from "./components/structures/navbar";
-import Teams from './components/home/team';
 
 function App() {
   return (
     <div>
-      <Teams/>
+      <Home/>
     </div>
   );
 }
--- src/components/home/about/index.js
+++ src/components/home/about/index.js
@@ -1,4 +1,3 @@
-import picture from './'
 import React from 'react'
 import './index.scss'
 import background from './images/planet.png'
@@ -15,7 +14,7 @@ const About = () => {
         <p className="tag">
           We built your feuture
         </p>
-        <p1 className="word">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+        <p className="word">Lorem Ipsum is simply dummy text of the printing and typesetting industry.
           Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type
           and scrambled it to make a type specimen book. It has survived not only five centuries but also the leap into electronic typesetting,
           remaining essentially unchanged.
@@ -25,7 +24,7 @@ const About = () => {
           <br /><br /><br />
           Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.
           It has survived not only five centuries but also the leap into electronic typesetting, remaining essentially unchanged.
-        </p1>
+        </p>
         <img className="phone-layers" src={phone} data-value="5" alt="phone" />
 
       </div>
