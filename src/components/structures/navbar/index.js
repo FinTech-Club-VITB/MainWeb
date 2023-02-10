223e953e2d796a5fc9a6eba21c5456a6cad624c1
Aditya Nandwana <you@example.com>
2023-02-10 23:16:01 +0530

    navbar

--- src/components/structures/navbar/index.js
+++ src/components/structures/navbar/index.js
@@ -1,10 +1,20 @@
 import React from 'react'
 import './index.scss'
+import logo from './logo.png'  
 
-const Navbar = () => {
+export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+
+        <div className="left"><img src={logo} alt=""/></div>
+        <div className="right">
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT US</li>
+                <li>CONTACT US</li>
+                <li>HOME</li>
+            </ul>
+        </div>
+    </nav>
   )
 }
-
-export default Navbar
