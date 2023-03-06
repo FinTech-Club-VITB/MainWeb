7c4b5df34dc97178b3b63b6b41d2def289d81b96
retsambew <namanaskn2@gmail.com>
2023-03-06 12:54:36 +0530

    navbar

--- src/components/home/hero/index.js
+++ src/components/home/hero/index.js
@@ -8,12 +8,12 @@ const Hero = () => {
     <>
       <div className='heroSection'>
         <div></div>
-        <div className='flexbox'>
+        <div className='infoSection'>
           <h1 className="heroHeading">FINTECH CLUB</h1>
-          <h3 className='heroSubheading'>DEFINING THE FUTURE OF <span className='finance'>FINANCE</span></h3>
+          <h3><span className='heroSubheading'>DEFINING THE FUTURE OF </span><span className='finance'>FINANCE</span></h3>
           <button className='heroJoinUs' role="button">JOIN NOW</button>
         </div>
-        <div className='flexbox'>
+        <div>
           <div className='social'>
             <a href=""><AiFillLinkedin/></a>
             <a href=""><AiFillTwitterSquare/></a>
--- src/components/structures/navbar/index.js
+++ src/components/structures/navbar/index.js
@@ -1,9 +1,18 @@
 import React from 'react'
 import './index.scss'
+import logo from "../../../assets/logo.svg"
 
 const Navbar = () => {
   return (
-    <div className='navbar'>Navbar</div>
+    <div className='navbar'>
+      <img src={logo} className="logo" alt=""/>
+      <div className="right">
+        <h2 className='link'>HOME</h2>
+        <h2 className='link'>EVENTS</h2>
+        <h2 className='link'>OUR TEAM</h2>
+        <h2 className='link'>RESOURCES</h2>
+      </div>
+    </div>
   )
 }
 
