c0fda67987c9a31c52fa759eaec746136b2b0975
retsambew <namanaskn2@gmail.com>
2023-04-22 22:19:36 +0530

    insta embed & Links

--- src/components/home/hero/index.js
+++ src/components/home/hero/index.js
@@ -1,22 +1,31 @@
-import React from 'react'
-import './index.scss'
-import Socials from '../../common/socials'
+import React from "react";
+import "./index.scss";
+import Socials from "../../common/socials";
 
 const Hero = () => {
   return (
     <>
-      <div className='bgimg'></div>
-      <div className='heroSection'>
-        <div className='infoSection'>
+      <div className="bgimg"></div>
+      <div className="heroSection">
+        <div className="infoSection">
           <h1 className="heroHeading">FINTECH CLUB</h1>
-          <h3><span className='heroSubheading'>DEFINING THE FUTURE OF </span><span className='finance'>FINANCE</span></h3>
-          <button className='heroJoinUs' role="button">JOIN NOW</button>
+          <h3>
+            <span className="heroSubheading">DEFINING THE FUTURE OF </span>
+            <span className="finance">FINANCE</span>
+          </h3>
+          <a
+            href="https://chat.whatsapp.com/FqAeqLFPVjA5Nl7hWgguq2"
+            target="_blank"
+          >
+            <button className="heroJoinUs" role="button">
+              JOIN NOW
+            </button>
+          </a>
         </div>
-        <Socials/>
+        <Socials />
       </div>
     </>
+  );
+};
 
-  )
-}
-
-export default Hero
+export default Hero;
